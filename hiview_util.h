284cfae3693a7fbb0f31e70c4dc6724bc741cf1e
huxia <huxia10@huawei.com>
2021-08-10 15:38:18 +0800

    Fix the frequent writing of hilog file headers and log current limit issues
    Signed-off-by: huxia <huxia10@huawei.com>
    Change-Id: I195784578dd9787821c9ba175c6e83f9da475863
    Signed-off-by: huxia <huxia10@huawei.com>

--- hiview_util.h
+++ hiview_util.h
@@ -78,7 +78,7 @@ int32 HIVIEW_FileClose(int32 handle);
 int32 HIVIEW_FileRead(int32 handle, uint8 *buf, uint32 len);
 int32 HIVIEW_FileWrite(int32 handle, const uint8 *buf, uint32 len);
 int32 HIVIEW_FileSeek(int32 handle, int32 offset, int32 whence);
-int32 HIVIEW_FileSize(const char *path);
+int32 HIVIEW_FileSize(int32 handle);
 int32 HIVIEW_FileUnlink(const char *path);
 int32 HIVIEW_FileCopy(const char *src, const char *dest);
 int32 HIVIEW_FileMove(const char *src, const char *dest);
